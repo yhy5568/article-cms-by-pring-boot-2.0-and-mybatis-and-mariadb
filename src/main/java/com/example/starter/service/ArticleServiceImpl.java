b9f88f3efcf9a754da0148387f194fded750b931
yhy <yhy5568@gmil.com>
2020-06-24 13:55:36 +0900

    CUtil 도입

--- src/main/java/com/example/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/example/starter/service/ArticleServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.starter.dao.ArticleDao;
 import com.example.starter.dto.Article;
+import com.example.starter.util.CUtil;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
@@ -26,10 +27,7 @@ public class ArticleServiceImpl implements ArticleService {
 	public long add(Map<String, Object> param) {
 		articleDao.add(param);
 		
-		BigInteger bigIntId = (BigInteger)param.get("id");
-		long newId = bigIntId.longValue();
-		
-		return newId;
+		return CUtil.getAsLong(param.get("id"));
 		
 	}
 }
